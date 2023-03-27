bbdeedec23dfe9c720c65ee8fad70d187225258e
Darya <daryaeremeeva2013@mail.ru>
2023-03-28 00:17:44 +0300

    Hi!

--- funcs.h
+++ funcs.h
@@ -1,8 +1,8 @@
-#ifndef __FUNCS_H__
+//Hi!
 #define __FUNCS_H__
 double MySum(double, double);
 double MySub(double, double);
 double MyMul(double, double);
 double MyDiv(double, double);
 double MyPow(double, double);
-#endif // __FUNCS_H__
+#endif // __FUNCS_H__
