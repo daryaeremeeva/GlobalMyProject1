598f9f3d89b445aa0d571c678f575fd25251899a
Darya <daryaeremeeva2013@mail.ru>
2023-03-27 20:27:36 +0300

    add function MyPow()

--- funcs.cpp
+++ funcs.cpp
@@ -10,7 +10,17 @@ double MySub(double a, double b)
 double MyMul(double a, double b)
 {
 	return a * b;
-}double MyDiv(double a, double b)
+}
+double MyDiv(double a, double b)
 {
 	return a / b;
-}
+}
+int MyPow(int num, int n)
+{
+	int res = num;
+	for (int i = 1; i < n; i++)
+	{
+		res *= num;
+	}
+	return res;
+}
--- funcs.h
+++ funcs.h
@@ -3,4 +3,6 @@
 double MySum(double, double);
 double MySub(double, double);
 double MyMul(double, double);
-#endif // __FUNCS_H__
+double MyDiv(double, double);
+int MyPow(int, int);
+#endif // __FUNCS_H__
